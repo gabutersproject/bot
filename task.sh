e6172df49f4b0ef707bb48d42a530ff8ee04c9f2
amier_san09 <73158213+MhmmdAmier@users.noreply.github.com>
2022-04-13 16:49:41 +0700

    Update task.sh

--- task.sh
+++ task.sh
@@ -16,4 +16,5 @@ echo "## 🤔 LAST UPDATED AT: ${d}" > update.md
 
 git config --local user.email "dewa27bkz@gmail.com"
 git config --local user.name "MhmmdAmier"
+git config --global --add safe.directory /github/workspace
 git commit -am "${arr[$rand]} (at ${d})"
